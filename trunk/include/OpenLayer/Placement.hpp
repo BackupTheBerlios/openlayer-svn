e905421aa235bbe4350e2d1365b5a18d86223e39
bradeeoh <bradeeoh@9f4572f3-6e0d-0410-ba3b-903ab02ac46b>
2006-03-03 23:18:49 +0000

    Overloaded Placement + Vec2D, Placement - Vec2D, and Placement + Placement

--- trunk/include/OpenLayer/Placement.hpp
+++ trunk/include/OpenLayer/Placement.hpp
@@ -14,63 +14,80 @@ public:
    Placement( Vec2D position = Vec2D( 0.0, 0.0 ),
               float rotation = 0.0,
               float stretch = 1.0 )          : position( position ), rotation( rotation ), stretch( stretch ) {}
-   
-   
+
+
    inline float GetDistance( const Placement &other ) const{
       return (position - other.position).GetMagnitude();
    }
-   
-   
+
+
    inline void MoveBy( Vec2D value ) {
       position += value;
    }
-   
-   
+
+
    inline void RotateBy( float value ) {
       rotation += value;
    }
-   
-   
+
+
    inline void StretchBy( float factor ) {
       stretch *= factor;
    }
-   
-   
+
+
    inline void SetPosition( Vec2D position ) {
       this->position = position;
    }
-   
-   
+
+
    inline Vec2D GetPosition() const {
       return position;
    }
-   
-   
+
+
    inline void SetRotation( float rotation ) {
       this->rotation = rotation;
    }
-   
-   
+
+
    inline float GetRotation() const {
       return rotation;
    }
-   
-   
+
+
    inline void SetStretch( float stretch ) {
       this->stretch = stretch;
    }
-   
-   
+
+
    inline float GetStretch() const {
       return stretch;
    }
-   
+
+   inline Placement& operator +( const Vec2D& amount ) {
+		position += amount;
+		return *this;
+   }
+
+   inline Placement& operator -( const Vec2D& amount ) {
+		position -= amount;
+		return *this;
+   }
+
+   inline Placement& operator +( const Placement& amount ) {
+		position += amount.position;
+		rotation += amount.rotation;
+		stretch *= amount.stretch;
+		return *this;
+   }
+
    // Returns the rotation and stretch in a matrix //
    Matrix2D Get2DMatrix() const;
-   
+
    // Applies the placement
    void Apply( const Vec2D &pivot ) const;
-   
+
 private:
    Vec2D position;
    float rotation;
